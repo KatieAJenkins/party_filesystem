86e73db0ae780ac5a17ee9dbfbbe386044a10df2
KatieAJenkins <kind2karma@gmail.com>
2016-11-08 16:56:17 -0700

    added a comment to atom

--- guests.js
+++ guests.js
@@ -4,11 +4,16 @@ var fs = require ('fs');
 var path = require ('path');
 var guestsPath = path.join(__dirname, 'guests.json');
 
+var node = path.basename(process.argv[0]);
+var file = path.basename(process.argv[1]);
+var cmd = process.argv[2];
+
 fs.readFile(guestsPath, 'utf8', function(err, data){
   if (err) {
     throw err;
   }
 
-  var guests = JSON.parse(data);
+  var guests = JSON.parse(data); //returns empty array
+
   console.log(guests);
 });
